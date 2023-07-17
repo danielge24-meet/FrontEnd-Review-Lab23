7164cae4dd73833fed1363d6e92cadd567c7f358
danielge24-meet <139704392+danielge24-meet@users.noreply.github.com>
2023-07-17 16:05:43 +0300

    i did the extra task

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function changeBackgroundColor(color){
-	document.body.style.background=color;
+function changeBackgroundColor(color1,color2){
+	if(document.body.style.background==color1)
+	{
+		document.body.style.background=color2;
+	}
+	else
+	{
+		document.body.style.background=color1;
+	}
+
 }
